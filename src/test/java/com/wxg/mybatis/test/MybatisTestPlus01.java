1cd2eb40c40b174e44727f4693b5486c69d38d6c
jason_glb <goodMorning_glb@jason.com>
2020-04-06 20:50:57 +0800

    新增延迟加载功能

--- src/test/java/com/wxg/mybatis/test/MybatisTestPlus01.java
+++ src/test/java/com/wxg/mybatis/test/MybatisTestPlus01.java
@@ -63,6 +63,13 @@ public class MybatisTestPlus01 {
     /**
      * 查询员工信息的同时查询部门信息
      * 使用association进行分步查询
+     * 分段查询的基础上可以实现延迟加载
+     * 分段查询的基础之上加上两个配置：全局配置中设置如下：
+     * 	 	<setting name="lazyLoadingEnabled" value="true"/>
+     * 	        lazyLoadingEnabled:延迟加载的全局开关。当开启时，所有关联对象都会延迟加载
+     *      <setting name="aggressiveLazyLoading" value="true"/> ,在 3.4.1 及之前的版本中默认为 true
+     *          aggressiveLazyLoading:开启时，任一方法的调用都会加载该对象的所有延迟加载属性。 否则，每个延迟加载属性会按需加载
+     *
      * @throws IOException
      */
     @Test
@@ -74,7 +81,7 @@ public class MybatisTestPlus01 {
             EmployeeMapperPlus mapper = sqlSession.getMapper(EmployeeMapperPlus.class);
             Employee employee = mapper.getEmpByIdStep(1);
             System.out.println(employee.getLastName());
-            System.out.println(employee.getDept());
+//            System.out.println(employee.getDept());
         }finally {
             sqlSession.close();
         }
